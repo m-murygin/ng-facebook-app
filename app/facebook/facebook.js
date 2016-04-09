c835d1f44bb02d9ef8c1c8a567351516f62c7bb4
Maksim Murygin <muriginm@gmail.com>
2016-04-09 12:51:17 +0600

    add controller

---
+++ app/facebook/facebook.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
+
+.config(['$routeProvider',function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  }); 
+}])
+
+.config(['$facebookProvider',function($facebookProvider) {
+  $facebookProvider.setAppId('1753491314883245');
+  $facebookProvider.setPermissions('email', 'public_profile', 'user_posts', 
+    'publish_actions', 'user_photos');
+}])
+
+.run( function($rootScope ) {
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = '//connect.facebook.net/en_US/sdk.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'))
+})
+
+.controller('FacebookCtrl', ['$scope', function($scope){
+  
+}]);
